91ccd243aaf6d9c14083cf17b61e7e7619e633ee
bruce <1330332817@qq.com>
2022-03-21 22:02:40 +0800

    first commit

---
+++ src/system_ros.cc
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "lidar_lam");
+    ros::NodeHandle nh;
+
+    ROS_INFO("Start to process data ...");
+
+    ROS::Subscriber 
+}
